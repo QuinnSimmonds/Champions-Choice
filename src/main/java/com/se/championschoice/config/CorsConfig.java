7cbafcf8ee2ce240fce07754bc15b0dee5036b80
QuinnSimmonds <qsimmonds55@gmail.com>
2025-10-19 16:53:00 -0400

    added cors config for react connection

---
+++ src/main/java/com/se/championschoice/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.se.championschoice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000") //need to change this to actual React URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
